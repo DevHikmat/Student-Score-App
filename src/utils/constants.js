6ce89eaba46460841c137903ca69e7c6d2a3cd00
DevHikmat <devhikmatgithub@gmail.com>
2024-08-15 02:54:56 +0500

    baseUrl changed from localhost to global

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://webstar-geamefecotion-1.onrender.com";
-export const BASE_URL = "http://localhost:4001/";
+export const BASE_URL = "https://webstar-geamefecotion-1.onrender.com";
+// export const BASE_URL = "http://localhost:4001/";
 
 export const ROLES = {
   STUDENT: "student",
